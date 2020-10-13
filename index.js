2b39261821eb826f9784aba294f46a2e6461cf7b
Luis <lgd26308@gmail.com>
2020-10-12 21:35:10 -0700

    Added funtions to write file and initialize program

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
-const util = require('utils')
+const api = require('./utils/githubapi.js');
+const util = require('util')
 
 const generateMarkdown = require('./utils/generateMarkdown.js')
 // array of questions for user
@@ -34,27 +35,35 @@ const questions = [
         message: 'Describe the installation process.',
         name: 'installation'
     },
+
     {
         type: 'list',
         message: "Choose a license for your project.",
         choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
         name: 'license'
     }
-    
+
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-    if (err) {
-        return console.log(err);
-      }
-    
-      console.log("Success!");
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("Success!");
+    });
 }
+
+const writeFileAsync = util.promisify(writeToFile);
 
 // function to initialize program
 function init() {
+    userInput = inquirer.prompt(questions)
+    userInfo = api.getUser(userInput)
 
+    writeFileAsync('ExampleREADME.md', generateMarkdown(userInput, userInfo))
 }
 
 // function call to initialize program
