514ec18fa034a09d8f405ffc883126c69793fcd0
Luis <lgd26308@gmail.com>
2020-10-12 20:10:49 -0700

    created list of questions to ask

--- index.js
+++ index.js
@@ -1,10 +1,55 @@
+const inquirer = require('inquirer')
+const fs = require('fs')
+const util = require('utils')
+
+const generateMarkdown = require('./utils/generateMarkdown.js')
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        message: 'Enter your github username.',
+        name: 'username'
+    },
 
+    {
+        type: 'input',
+        message: 'Enter your github repo name.',
+        name: 'repo'
+    },
+
+    {
+        type: 'input',
+        message: 'Enter your project name.',
+        name: 'Title'
+    },
+
+    {
+        type: 'input',
+        message: 'Give a description of the project',
+        name: 'description'
+    },
+
+    {
+        type: 'input',
+        message: 'Describe the installation process.',
+        name: 'installation'
+    },
+    {
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license'
+    }
+    
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    if (err) {
+        return console.log(err);
+      }
+    
+      console.log("Success!");
 }
 
 // function to initialize program
